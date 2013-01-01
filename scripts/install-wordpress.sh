40bfeaf1dac706f63d8e78aa5699e51185b9cc61
mattmcclean <matt.mcclean@gmail.com>
2013-01-01 15:32:51 +0000

    added wordpress config params

--- scripts/install-wordpress.sh
+++ scripts/install-wordpress.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+# update packages
 yum -y update
+# install Apache and PHP
 yum -y install httpd
 yum -y install php php-mysql
+
+# Install Wordpress
+cd /var/www/html
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+mv wordpress blog
+
+cd blog
+mv wp-config-sample.php wp-config.php
+sed -i 's/database_name_here/$DB_NAME/g' wp-config.php
+sed -i 's/username_here/$DB_USERNAME/g' wp-config.php
+sed -i 's/password_here/$DB_PASSWORD/g' wp-config.php
+sed -i 's/localhost/$DB_HOST/g' wp-config.php
 
 service httpd start
 chkconfig httpd on 
