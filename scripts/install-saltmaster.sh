f458f7d8f30a6fda6ca9b305c0912b3967323c5e
Ubuntu <ubuntu@ip-10-227-51-108.eu-west-1.compute.internal>
2013-01-28 18:43:47 +0000

    added git clone to saltmaster

--- scripts/install-saltmaster.sh
+++ scripts/install-saltmaster.sh
@@ -5,6 +5,12 @@ add-apt-repository ppa:saltstack/salt
 
 # update
 apt-get update
+apt-get upgrade -y
 
 # install the salt master
 apt-get -y install salt-master
+
+cd /home/ubuntu
+git clone https://github.com/mattmcclean/mmcclean-salt-config.git
+
+ln -s /home/ubuntu/mmcclean-salt-config/salt /srv/salt
