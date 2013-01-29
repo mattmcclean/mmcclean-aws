3f7e7c19b8e335f3d0b498ea69816bf3ed9c530c
Matt McClean <matt.mcclean@gmail.com>
2013-01-29 12:44:07 +0000

    added sed command to insert salt master fqdn

--- scripts/install-saltminion.sh
+++ scripts/install-saltminion.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# add the salt repository
-add-apt-repository ppa:saltstack/salt
+add-apt-repository ppa:saltstack/salt -y
+apt-get update -y
+apt-get install salt-minion -y
+apt-get install salt-master -y
+apt-get upgrade -y
 
-# update
-apt-get update
-
-# install the salt master
-apt-get -y install salt-minion
+echo "Setting salt master to " $SALT_MASTER
+# Set salt master location and start minion
+sed -i 's/#master: salt/master: '$SALT_MASTER'/' /etc/salt/minion
+salt-minion -d
