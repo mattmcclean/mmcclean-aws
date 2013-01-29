bee1dd5bcd845898dd2b9127f52417c627e4b2b9
Matt McClean <matt.mcclean@gmail.com>
2013-01-29 12:48:20 +0000

    added check for variable

--- scripts/install-saltminion.sh
+++ scripts/install-saltminion.sh
@@ -6,7 +6,9 @@ apt-get install salt-minion -y
 apt-get install salt-master -y
 apt-get upgrade -y
 
-echo "Setting salt master to " $SALT_MASTER
-# Set salt master location and start minion
-sed -i 's/#master: salt/master: '$SALT_MASTER'/' /etc/salt/minion
-salt-minion -d
+if [ -z "$SALT_MASTER" ]; then
+    echo "Setting salt master to " $SALT_MASTER
+    # Set salt master location and start minion
+    sed -i 's/#master: salt/master: '$SALT_MASTER'/' /etc/salt/minion
+    service salt-minion restart
+fi
